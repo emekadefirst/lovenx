110e014a141109038938c2075af14db179b82d17
emekadefirst <victorchibuohwu33@gmail.com>
2025-09-02 11:31:44 +0100

    templates

--- xapp/urls.py
+++ xapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, login_view, signup_view, profile
 
 
 urlpatterns = [    
     path('', home, name='home'),
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+    path('profile/', profile, name='profile'),
 ]
--- xapp/views.py
+++ xapp/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def login_view(request):
+    return render(request, 'login.html')    
+
+
+def signup_view(request):
+    return render(request, 'signup.html')
+
+
+
+def profile(request):
+    return render(request, 'profile.html')
